285c469329cd496eed15b17960234a67a82bcf7a
Apothous <dgappjr@gmail.com>
2025-05-20 13:00:01 -0500

    Delete GetBTDeviceStatus.ps1

--- GetBTDeviceStatus.ps1
+++
@@ -1,6 +0,0 @@
-﻿Get-PnpDevice -Class AudioEndpoint | ForEach-Object {
-    Write-Host "FriendlyName: $($_.FriendlyName)"
-    Write-Host "Status: $($_.Status)"
-    Write-Host "InstanceId: $($_.InstanceId)"
-    Write-Host "------------------------------------"
-}
